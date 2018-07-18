6d6c1a674e70f738200ef902e43e1daad19adf27
Nicolas Gross <nicolas@fam-gross.org>
2018-07-18 15:49:37 +0200

    Finish main screen

--- src/main/java/de/nicolasgross/wcttt/gui/Main.java
+++ src/main/java/de/nicolasgross/wcttt/gui/Main.java
@@ -32,7 +32,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
 
-		Scene scene = new Scene(root, 800, 600);
+		Scene scene = new Scene(root, 1000, 600);
 
 		primaryStage.setTitle("WIAI Course Timetabling Tool");
 		primaryStage.setScene(scene);
