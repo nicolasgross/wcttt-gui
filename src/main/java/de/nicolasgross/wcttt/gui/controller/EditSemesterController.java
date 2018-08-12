9b5126456ea350c49ff63c5128db3000dc82d641
Nicolas Gross <nicolas@fam-gross.org>
2018-08-12 18:57:16 +0200

    Add edit semester fxml mappings

--- src/main/java/de/nicolasgross/wcttt/gui/controller/EditSemesterController.java
+++ src/main/java/de/nicolasgross/wcttt/gui/controller/EditSemesterController.java
@@ -1,5 +1,42 @@
 package de.nicolasgross.wcttt.gui.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
 public class EditSemesterController extends Controller {
+
+	@FXML
+	private TextField nameField;
+	@FXML
+	private ChoiceBox<Integer> daysPerWeekBox;
+	@FXML
+	private ChoiceBox<Integer> timeSlotsBox;
+	@FXML
+	private TextField maxLecturesField;
+
+	@FXML
+	private TextField s1Field;
+	@FXML
+	private TextField s2Field;
+	@FXML
+	private TextField s3Field;
+	@FXML
+	private TextField s4Field;
+	@FXML
+	private TextField s5Field;
+	@FXML
+	private TextField s6Field;
+	@FXML
+	private TextField s7Field;
+	@FXML
+	private TextField s8Field;
+
+	@FXML
+	private Button cancelButton;
+	@FXML
+	private Button applyButton;
+
 
 }
