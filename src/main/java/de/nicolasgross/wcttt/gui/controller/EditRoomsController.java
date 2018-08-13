e478bbd6dc59f30178b13b15aecd4f60ddb9ff2d
Nicolas Gross <nicolas@fam-gross.org>
2018-08-13 19:25:23 +0200

    Add room list elements

--- src/main/java/de/nicolasgross/wcttt/gui/controller/EditRoomsController.java
+++ src/main/java/de/nicolasgross/wcttt/gui/controller/EditRoomsController.java
@@ -1,9 +1,14 @@
 package de.nicolasgross.wcttt.gui.controller;
 
-import de.nicolasgross.wcttt.lib.model.Chair;
-import de.nicolasgross.wcttt.lib.model.Room;
+import de.nicolasgross.wcttt.gui.model.Model;
+import de.nicolasgross.wcttt.lib.model.*;
+import javafx.application.Platform;
+import javafx.collections.ListChangeListener;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.util.List;
 
 public class EditRoomsController extends Controller {
 
@@ -29,4 +34,44 @@ public class EditRoomsController extends Controller {
 	private CheckBox docCamCheckBox;
 	@FXML
 	private Button applyButton;
+
+	@FXML
+	protected void initialize() {
+		addRoomButton.setOnAction(event -> {
+			try {
+				getModel().getSemester().addInternalRoom(new InternalRoom());
+			} catch (WctttModelException e) {
+				e.printStackTrace();
+				// TODO
+			}
+		});
+	}
+
+	@Override
+	public void setup(Stage stage, Model model, MainController mainController) {
+		super.setup(stage, model, mainController);
+		mergeInternalAndExternalRooms();
+
+		getModel().getInternalRooms().addListener(
+				(ListChangeListener<InternalRoom>) c ->
+						mergeInternalAndExternalRooms());
+
+		getModel().getExternalRooms().addListener(
+				(ListChangeListener<ExternalRoom>) c ->
+						mergeInternalAndExternalRooms());
+	}
+
+	private void mergeInternalAndExternalRooms() {
+		@SuppressWarnings("unchecked")
+		List<Room> internalRooms = (List<Room>) (List<? extends Room>)
+				getModel().getInternalRooms();
+		@SuppressWarnings("unchecked")
+		List<Room> externalRooms = (List<Room>) (List<? extends Room>)
+				getModel().getExternalRooms();
+		Platform.runLater(() -> {
+			roomListView.getItems().clear();
+			roomListView.getItems().addAll(internalRooms);
+			roomListView.getItems().addAll(externalRooms);
+		});
+	}
 }
