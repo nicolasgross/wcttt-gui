c70b6fce5464eece7dc3bb89d99bf39e6ad4be59
Nicolas Gross <nicolas@fam-gross.org>
2018-08-26 13:20:43 +0200

    Add license headers

--- src/main/java/de/nicolasgross/wcttt/gui/controller/MainFiltersController.java
+++ src/main/java/de/nicolasgross/wcttt/gui/controller/MainFiltersController.java
@@ -1,3 +1,27 @@
+/*
+ * WCT³ (WIAI Course Timetabling Tool) is a software that strives to automate
+ * the timetabling process at the WIAI faculty of the University of Bamberg.
+ *
+ * WCT³ GUI comprises functionality to view generated timetables, edit semester
+ * data and to generate new timetables.
+ *
+ * Copyright (C) 2018 Nicolas Gross
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package de.nicolasgross.wcttt.gui.controller;
 
 import de.nicolasgross.wcttt.gui.model.Model;
